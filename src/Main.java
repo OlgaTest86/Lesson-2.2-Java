aafdcf221e8079fa40670e9540aea66cd437c6d2
Dudkina Olga <dudkinaolia@yandex.ru>
2021-10-20 21:02:39 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAccount = 200;
+        int refill = 100;
+        int bonus;
+        if (refill <= 100) {
+            bonus = 0;
+        }else bonus = refill / 100;
+        int totalSum = currentAccount + refill + bonus;
+
+
+
+
+        System.out.println(totalSum);
+    }
+}
